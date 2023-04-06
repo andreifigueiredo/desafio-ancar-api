0be8c76d3471775e95a01bd5fbf28e27beb5423d
Andrei <afigueiredo@gyramais.com>
2023-04-06 12:34:34 -0300

    user-module

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
+import { USER_REPOSITORY } from '../../core/constants';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
+  ) {}
+
+  async create(user: UserDto): Promise<User> {
+    return await this.userRepository.create<User>(user);
+  }
+
+  async findOneById(id: number): Promise<User> {
+    return await this.userRepository.findOne<User>({ where: { id } });
+  }
+}
